23596c790717df7b1d5be5c35d3113a65a8f90ea
Edgar Thornton <thornton.edgar@gmail.com>
2025-10-28 15:28:42 -0600

    removeuv

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
 set -e
 
 echo "Installing dependencies with uv..."
-uv pip install -r requirements.txt --python "$(which python)"
+pip install -r requirements.txt 
 
 
 echo "Running migrations..."
