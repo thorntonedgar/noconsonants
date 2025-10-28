ab9ae7e9197f2b8075a2a542f5b79da934b958f5
Edgar Thornton <thornton.edgar@gmail.com>
2025-10-28 15:16:32 -0600

    Update build script to use uv for dependency installation

--- build.sh
+++ build.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 # Build script for production deployment
 
+set -e
+
+echo "Installing dependencies with uv..."
+uv pip install -r requirements.txt
+
 echo "Running migrations..."
 python manage.py migrate --no-input
 
